c19d17312e795efd085d29c18de7751ca4ac8ecd
Michael303pt <miguelamorimconceicao@gmail.com>
2026-07-15 15:14:48 +0100

    login feito
    yes

---
+++ login.js
@@ -0,0 +1,20 @@
+async function login(){
+
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const resposta=await fetch("/api/login",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({email, password})
+    });
+
+    const dados=await resposta.json();
+    if(resposta.ok){
+        localStorage.setItem("token", dados.token);
+        window.location="index.html";
+    }else{
+        alert(dados.erro);
+    }
+}
